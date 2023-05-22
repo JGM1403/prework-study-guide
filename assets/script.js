9ae11bb8b6c1b35c23989be0f5d764fb38adb11c
JGM1403 <134123546+JGM1403@users.noreply.github.com>
2023-05-22 02:34:13 -0400

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("let's study HTML!");
+    } else if (randomTopic === 'CSS'){
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git'){
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'Javascript'){
+        console.log("Let's study Javascript!");
+    } else {
+        console.log('please try again!');
+    }
+}
+console.log('here are the topics we learned through prework:');
+listTopics();
+console.log('which topic should we study first?');
+selectTopic();
